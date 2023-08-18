84d6ba7676c32cabc0d1a9034e948738c25085fd
Mark Cookson <cookson.mark@gmail.com>
2023-08-18 22:29:08 +1200

    Initial commit

---
+++ Chapter02/helloWorld.cpp
@@ -0,0 +1,11 @@
+// Programming Principles and Practice 2.2, pg 45
+// Classic first program
+// This program prints 'Hello World' to the console
+
+#include "std_lib_facilities.h"
+
+int main()
+{
+    cout << "Hello, World!\n";
+    return 0;
+}
