8c23102bf4700db9670df08bef7cd4c0edc7ece2
Mark Cookson <cookson.mark@gmail.com>
2023-10-24 12:23:31 +1300

    Completed Chapter 4, Exercise 4

--- 4/4_Ex4.cpp
+++ 4/4_Ex4.cpp
@@ -5,9 +5,46 @@
 
 int main()
 {
-    cout << "Please enter an integer from 1 to 100: ";
+    int min = 1;
+    int max = 100;
     int answer;
-    cin >> answer;
+
+    cout << "Please enter an integer from " << min << " to " << max << ":\n";
+    for (int temp; cin >> temp;)
+    {
+        if (temp >= 1 && temp <= 100)
+        {
+            answer = temp;
+            break;
+        }
+        cout << "Please enter an integer from " << min << " to " << max << ":\n";
+    }
+
+    int guess = 50;
+
+
+    while (guess != answer)
+    {
+        cout << "Is the number you are thinking of less than " << guess << "? Enter y or n.\n";
+        char temp;
+        cin >> temp;
+
+        if (temp == 'y')
+        {
+            max = guess;
+            guess = (min + guess) / 2;
+
+        }
+        if (temp == 'n')
+        {
+            min = guess;
+            guess = (max + guess) / 2;
+
+        }
+    }
+
+    cout << "The number you entered was " << guess << ".\n";
+
 }
 
 
