b79a4e5b3264c7bcae81c33f46661d0940cfc3b5
Mark Cookson <cookson.mark@gmail.com>
2024-03-28 23:36:45 +1300

    Ch 6 Ex 6 added alternative permutations2 function to allow larger calculations without overflow

--- 6/6_ex10.cpp
+++ 6/6_ex10.cpp
@@ -1,5 +1,10 @@
 // Programming: Principles and Practice Using C++ (2nd ed) Chapter 6, Exercise 10
 // Read two integers, calculate either permutations or combinations for those two numbers
+
+// 28/3/24 Added 'permutations2' function, which simplifies the division for permutation calculation
+// and allows eg P(24, 3) which overflows the original permutations function, as any calculating any factorial
+// larger than 20 will overflow a long int.
+
 #include <std_lib_facilities.h>
 
 long factorial (long a)
@@ -18,6 +23,17 @@ long permutations(long a, long b)
     return factorial(a) / factorial(a-b);
 }
 
+long permutations2(long a, long b)
+{
+    long total = 1;
+    for (int x=a; x > a - b; x--)
+    {
+        total *= x;
+        if (total < 0) error("Factorial overflow in permutations2");
+    }
+    return total;
+
+}
 long combinations(long a, long b)
 {
     return permutations(a, b) / factorial(b);
@@ -66,7 +82,7 @@ try
 
     if(toupper(operation) == 'P')
     {
-        long answer = permutations(a,b);
+        long answer = permutations2(a,b);
         cout << "There are " << answer << " possible permutations for ";
         cout << b << " out of " << a << " numbers.\n";
         exit(0);
